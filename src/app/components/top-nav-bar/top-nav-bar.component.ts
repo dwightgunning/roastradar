fafbcd81e33db91843504cd8d96a440cd0db5ddd
Dwight Gunning <dwight@dwightgunning.com>
2019-04-01 15:40:11 +0200

    Removed dead code

--- src/app/components/top-nav-bar/top-nav-bar.component.ts
+++ src/app/components/top-nav-bar/top-nav-bar.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-top-nav-bar',
   templateUrl: './top-nav-bar.component.html',
   styleUrls: ['./top-nav-bar.component.scss']
 })
-export class TopNavBarComponent implements OnInit {
+export class TopNavBarComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
