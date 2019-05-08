c2e9e287e329d731581f3e6b9e41624c84dab0ff
Dwight Gunning <dwight@dwightgunning.com>
2019-05-08 16:15:42 +0200

    Refactored connectivity service to expose a multicast Subject

--- src/app/services/connectivity/connectivity.service.spec.ts
+++ src/app/services/connectivity/connectivity.service.spec.ts
@@ -1,84 +1,132 @@
 import { TestBed } from '@angular/core/testing';
 
+import { BehaviorSubject } from 'rxjs';
+
 import { ConnectivityService } from './connectivity.service';
 
 describe('ConnectivityService', () => {
-  let onlineState;
   let addEventListenerSpy;
   let navigatorOnlineSpy;
-  let listeners;
+  let removeEventListenerSpy;
+  let windowListeners;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     addEventListenerSpy = spyOn(window, 'addEventListener');
+    removeEventListenerSpy = spyOn(window, 'removeEventListener');
     navigatorOnlineSpy = spyOnProperty(window.navigator, 'onLine');
-    listeners = {};
-    addEventListenerSpy.and.callFake((event, listener) => {
-      if (!listeners[event]) {
-        listeners[event] = [listener];
+    windowListeners = {};
+    addEventListenerSpy.and.callFake((event, windowListener) => {
+      if (!windowListeners[event]) {
+        windowListeners[event] = [windowListener];
       } else {
-        listeners[event].append(listener);
+        windowListeners[event].append(windowListener);
+      }
+    });
+    removeEventListenerSpy.and.callFake((event, windowListener) => {
+      if (event !== 'load') {
+        if (!windowListeners[event]) {
+          fail(`unexpected call to window.removeEventListener: ${event}`);
+        } else {
+          windowListeners[event].splice(windowListeners[event].indexOf(windowListener), 1);
+        }
       }
     });
   });
 
-  afterEach(() => {
+  it('should be created without adding listeners or retrieving online state', () => {
+    const service: ConnectivityService = TestBed.get(ConnectivityService);
+    expect(navigatorOnlineSpy).toHaveBeenCalledTimes(0);
+    expect(addEventListenerSpy).toHaveBeenCalledTimes(0);
+  });
+
+  it('emits initial connectivity state (online) from \'navigator.onLine\' when subscribed to', (onExpectationsMet) => {
+    const onlineState = true;
+    navigatorOnlineSpy.and.returnValue(onlineState);
+    const service: ConnectivityService = TestBed.get(ConnectivityService);
+
+    const connectivitySub = service.connectivity().subscribe((online: boolean) => {
+      expect(online).toBeTruthy();
+      onExpectationsMet();
+    });
     expect(navigatorOnlineSpy).toHaveBeenCalledTimes(1);
     expect(addEventListenerSpy).toHaveBeenCalledTimes(2);
-    expect(addEventListenerSpy).toHaveBeenCalledWith('offline', jasmine.any(Function));
-    expect(addEventListenerSpy).toHaveBeenCalledWith('online', jasmine.any(Function));
+
+    connectivitySub.unsubscribe();
+    expect(removeEventListenerSpy).toHaveBeenCalledTimes(2);
   });
 
-  it('should be created (online)', (onExpectationsMet) => {
-    onlineState = true;
+  it('emits initial connectivity state (offline) from \'navigator.onLine\' when first subscribed to', (onExpectationsMet) => {
+    const onlineState = false;
     navigatorOnlineSpy.and.returnValue(onlineState);
     const service: ConnectivityService = TestBed.get(ConnectivityService);
 
-    service.connectivity().subscribe((online: boolean) => {
-      expect(online).toBe(onlineState);
+    const connectivitySub = service.connectivity().subscribe((online: boolean) => {
+      expect(online).toBeFalsy();
       onExpectationsMet();
     });
+    expect(navigatorOnlineSpy).toHaveBeenCalledTimes(1);
+    expect(addEventListenerSpy).toHaveBeenCalledTimes(2);
+
+    connectivitySub.unsubscribe();
+    expect(removeEventListenerSpy).toHaveBeenCalledTimes(2);
   });
 
-  it('should be created (offline)', (onExpectationsMet) => {
-    onlineState = false;
-    navigatorOnlineSpy.and.returnValue(onlineState);
-    const service: ConnectivityService = TestBed.get(ConnectivityService);
-
-    service.connectivity().subscribe((online: boolean) => {
-      expect(online).toBe(onlineState);
-      onExpectationsMet();
-    });
-  });
-
-  it('emits online events', (onExpectationsMet) => {
+  it('emits connectivity changes recieved via window event listeners', (onExpectationsMet) => {
     const initialNavigatorState = false;
-    const updatedConnectivityEvent = 'online';
+    const updatedConnectivityEvents = ['online', 'offline', 'offline', 'online'];
     navigatorOnlineSpy.and.returnValue(initialNavigatorState);
     const service: ConnectivityService = TestBed.get(ConnectivityService);
-    listeners[updatedConnectivityEvent].forEach((listener) => listener());
 
-    service.connectivity().subscribe(
-      (online) => {
-        expect(online).toBe(true);
+    const expectedConnectivityStates = [false, true, false, false, true];
+
+    const connectivitySub = service.connectivity().subscribe(
+      (online: boolean) => {
+        expect(online).toEqual(expectedConnectivityStates.shift());
         onExpectationsMet();
       }
     );
+    updatedConnectivityEvents.forEach((connectivityEvent) => {
+      windowListeners[connectivityEvent].forEach((windowListener) => {
+        windowListener();
+      });
+    });
+    connectivitySub.unsubscribe();
+    expect(removeEventListenerSpy).toHaveBeenCalledTimes(2);
   });
 
-  it('emits offline events', (onExpectationsMet) => {
-    const initialNavigatorState = true;
-    const updatedConnectivityEvent = 'offline';
+  it('emits connectivity changes to multiple subscribers', (onExpectationsMet) => {
+    const initialNavigatorState = false;
+    const updatedConnectivityEvents = ['online', 'offline', 'offline', 'online'];
     navigatorOnlineSpy.and.returnValue(initialNavigatorState);
     const service: ConnectivityService = TestBed.get(ConnectivityService);
-    listeners[updatedConnectivityEvent].forEach((listener) => listener());
 
-    service.connectivity().subscribe(
-      (online) => {
-        expect(online).toBe(false);
+    const expectedConnectivityStates1 = [false, true, false, false, true];
+    const connectivitySub1 = service.connectivity().subscribe(
+      (online: boolean) => {
+        expect(online).toEqual(expectedConnectivityStates1.shift());
+        // TODO: Confirm expectations met from both async subscriptions
+      }
+    );
+
+    const expectedConnectivityStates2 = [false, true, false, false, true];
+    const connectivitySub2 = service.connectivity().subscribe(
+      (online: boolean) => {
+        expect(online).toEqual(expectedConnectivityStates2.shift());
         onExpectationsMet();
       }
     );
+    expect(navigatorOnlineSpy).toHaveBeenCalledTimes(1);
+    expect(addEventListenerSpy).toHaveBeenCalledTimes(2);
+
+    updatedConnectivityEvents.forEach((connectivityEvent) => {
+      windowListeners[connectivityEvent].forEach((windowListener) => {
+        windowListener();
+      });
+    });
+    connectivitySub1.unsubscribe();
+    connectivitySub2.unsubscribe();
+    expect(removeEventListenerSpy).toHaveBeenCalledTimes(2);
   });
 
 });
--- src/app/services/connectivity/connectivity.service.ts
+++ src/app/services/connectivity/connectivity.service.ts
@@ -1,30 +1,51 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+
+import { ConnectableObservable, Observable } from 'rxjs';
+import { multicast, publishReplay } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConnectivityService {
-  private monitor: Observable<boolean>;
-  private subject: BehaviorSubject<boolean>;
+  public static readonly OFFLINE_EVENT = 'offline';
+  public static readonly ONLINE_EVENT = 'online';
+  private connectivityMonitor: Observable<boolean>;
+  private connectivityObserver: ConnectableObservable<boolean>;
 
-  constructor() {
-    this.monitor = new Observable((connectivityObserver) => {
-      // map 'offline' and 'online' window events to boolean 'connnectivity state'
-      window.addEventListener('offline', () => {
-        connectivityObserver.next(false);
-      });
-      window.addEventListener('online', () => {
-        connectivityObserver.next(true);
-      });
-    });
-    // obtain initial connectivity state from navigator.onLine property
-    this.subject = new BehaviorSubject(navigator.onLine);
-    this.monitor.subscribe(this.subject);
+  constructor() { }
+
+  initConnectivityObserver() {
+    this.connectivityMonitor = new Observable(
+      (connectivityStateObserver) => {
+        const offlineEventListener = () => {
+          connectivityStateObserver.next(false); // Browser offline -> connectivity: false
+        };
+        const onlineEventListener = () => {
+          connectivityStateObserver.next(true); // Browser online -> connectivity: true
+        };
+
+        window.addEventListener(ConnectivityService.OFFLINE_EVENT, offlineEventListener);
+        window.addEventListener(ConnectivityService.ONLINE_EVENT, onlineEventListener);
+
+        // Emit the current connectivity
+        connectivityStateObserver.next(navigator.onLine);
+        return () => {
+          window.removeEventListener(ConnectivityService.OFFLINE_EVENT, offlineEventListener);
+          window.removeEventListener(ConnectivityService.ONLINE_EVENT, onlineEventListener);
+        };
+      }
+    );
+
+    // Publish a multicast ReplaySubject that emits the most recent connectivity status
+    this.connectivityObserver = this.connectivityMonitor.pipe(publishReplay(1)) as ConnectableObservable<boolean>;
+    this.connectivityObserver.connect();
   }
 
   connectivity(): Observable<boolean> {
-    return this.subject.asObservable();
-    // TODO: Proper cleanup?
+    if (!this.connectivityObserver) {
+      this.initConnectivityObserver();
+    }
+    return this.connectivityObserver.refCount();
   }
 }
