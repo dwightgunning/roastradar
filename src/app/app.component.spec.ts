c6d6a66ac1980db72d37c066b56a5b6bafee942b
Dwight Gunning <dwight@dwightgunning.com>
2019-05-09 23:47:44 +0200

    Simplified AppComponent unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,10 @@
 import { APP_BASE_HREF } from '@angular/common';
+import { Component } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { RouterTestingModule } from '@angular/router/testing';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AgmCoreModule } from '@agm/core';
-import { AgmJsMarkerClustererModule } from '@agm/js-marker-clusterer';
 import { NgcCookieConsentModule, NgcCookieConsentConfig } from 'ngx-cookieconsent';
 
-import { AboutComponent } from './components/screens/about/about.component';
 import { AppComponent } from './app.component';
-import { ContributeComponent } from './components/screens/contribute/contribute.component';
-import { environment } from '../environments/environment';
-import { FooterBarComponent } from './components/footer-bar/footer-bar.component';
-import { HomeComponent } from './components/screens/home/home.component';
-import { RoasterDetailsComponent } from './components/roaster-details/roaster-details.component';
-import { TermsPrivacyComponent } from './components/screens/terms-privacy/terms-privacy.component';
-import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.component';
-import { EncodeURIComponentPipe } from './pipes/encode-uricomponent.pipe';
 
 const cookieConfig: NgcCookieConsentConfig = {
   cookie: {
@@ -25,29 +12,27 @@ const cookieConfig: NgcCookieConsentConfig = {
   }
 };
 
+@Component({selector: 'app-top-nav-bar', template: ''})
+class TopNavBarStubComponent {}
+
+@Component({selector: 'app-footer-bar', template: ''})
+class FooterBarStubComponent {}
+
+// tslint:disable-next-line:component-selector
+@Component({selector: 'router-outlet', template: ''})
+class RouterOutletStubComponent { }
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        AppRoutingModule,
-        AgmCoreModule.forRoot({
-          apiKey: environment.GOOGLE_MAPS_API_KEY
-        }),
-        AgmJsMarkerClustererModule,
-        FormsModule,
-        RouterTestingModule,
         NgcCookieConsentModule.forRoot(cookieConfig)
       ],
       declarations: [
         AppComponent,
-        ContributeComponent,
-        HomeComponent,
-        TopNavBarComponent,
-        FooterBarComponent,
-        AboutComponent,
-        EncodeURIComponentPipe,
-        RoasterDetailsComponent,
-        TermsPrivacyComponent
+        FooterBarStubComponent,
+        TopNavBarStubComponent,
+        RouterOutletStubComponent
       ],
       providers: [
         {provide: APP_BASE_HREF, useValue: '/'}
@@ -58,19 +43,8 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Roast Radar'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Roast Radar');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Roast Radar');
-  });
 });
